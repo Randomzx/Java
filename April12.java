a0cf7eba9402108000d8cc39732615e3871e3990
sp6458 <sp645@hotmail.com>
2024-04-12 21:03:47 -0400

    april12

---
+++ April12.java
@@ -0,0 +1,88 @@
+package derived;
+
+public class April12 {
+}
+
+// demonstrates which class members of OuterClass an inner class (InnerClass),
+//  a nested static class (StaticNestedClass), and a top-level class (TopLevelClass) can access
+ class OuterClass {
+
+    String outerField = "Outer field";
+    static String staticOuterField = "Static outer field";
+
+    class InnerClass {
+        void accessMembers() {
+            System.out.println(outerField);
+            System.out.println(staticOuterField);
+        }
+    }
+
+    static class StaticNestedClass {
+        void accessMembers(OuterClass outer) {
+            // Compiler error: Cannot make a static reference to the non-static
+            //     field outerField
+            // System.out.println(outerField);
+            System.out.println(outer.outerField);
+            System.out.println(staticOuterField);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Inner class:");
+        System.out.println("------------");
+        OuterClass outerObject = new OuterClass();
+        OuterClass.InnerClass innerObject = outerObject.new InnerClass();
+        innerObject.accessMembers();
+
+        System.out.println("\nStatic nested class:");
+        System.out.println("--------------------");
+        StaticNestedClass staticNestedObject = new StaticNestedClass();
+        staticNestedObject.accessMembers(outerObject);
+
+        System.out.println("\nTop-level class:");
+        System.out.println("--------------------");
+        TopLevelClass topLevelObject = new TopLevelClass();
+        topLevelObject.accessMembers(outerObject);
+    }
+}
+
+// Note that a static nested class interacts with the instance members of its outer class just like any other top-level class.
+// The static nested class StaticNestedClass can't directly access outerField because it's an instance variable
+// of the enclosing class, OuterClass. The Java compiler generates an error at the highlighted statement:
+class TopLevelClass {
+
+    void accessMembers(OuterClass outer) {
+        // Compiler error: Cannot make a static reference to the non-static
+        //     field OuterClass.outerField
+        // System.out.println(OuterClass.outerField);
+        System.out.println(outer.outerField);
+        System.out.println(OuterClass.staticOuterField);
+    }
+}
+
+// If a declaration of a type (such as a member variable or a parameter name) in a particular
+// scope (such as an inner class or a method definition) has the same name as another declaration
+// in the enclosing scope, then the declaration shadows the declaration of the enclosing scope.
+// You cannot refer to a shadowed declaration by its name alone. The following example, ShadowTest,
+// demonstrates this:
+class ShadowTest {
+
+    public int x = 0;
+
+    class FirstLevel {
+
+        public int x = 1;
+
+        void methodInFirstLevel(int x) {
+            System.out.println("x = " + x);
+            System.out.println("this.x = " + this.x);
+            System.out.println("ShadowTest.this.x = " + ShadowTest.this.x);
+        }
+    }
+
+    public static void main(String... args) {
+        ShadowTest st = new ShadowTest();
+        ShadowTest.FirstLevel fl = st.new FirstLevel();
+        fl.methodInFirstLevel(23);
+    }
+}
